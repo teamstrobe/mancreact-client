9843442d5aefcf9dd106e7e592c35694bd8e4286
Joe Critchley <joecritch@gmail.com>
2017-04-21 14:53:32 +0100

    Stuff

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
 // import UpcomingEventHero from './UpcomingEventHero';
 import PreviousMeetupsList from './PreviousMeetupsList' ;
+import { mockEventsList } from '../mocks/events';
 
-const HomeScreen = () => (
-  <div>
-    {/* <UpcomingEventHero /> */}
-    <PreviousMeetupsList />
-  </div>
-);
+const HomeScreen = () => {
+  const events = mockEventsList;
+  // let upcomingEvent;
+  let previousEvents;
+  if (events != null) {
+    // upcomingEvent = events.filter(event => event.status === 'upcoming')[0];
+    previousEvents = events.filter(event => event.status === 'past');
+  }
+  return (
+    <div>
+      {/* <UpcomingEventHero /> */}
+      <PreviousMeetupsList events={previousEvents} />
+    </div>
+  );
+};
 
 export default HomeScreen;
--- src/components/PreviousMeetupsList.js
+++ src/components/PreviousMeetupsList.js
@@ -1,43 +1,38 @@
 import React from 'react';
+import { groupBy } from 'lodash';
+import moment from 'moment';
 
-const PreviousMeetupsList = () => (
-
-	<section>
-	  <h2 className="label prev-list-title">Previous meetups</h2>
-	  <div className="prev-list-group">
-		<div className="prev-list-group__item">
-		  <h3>2017</h3>
-		  <ul className="prev-list">
-			<li>
-			  <a href="?event=1">
-				April
-			  </a>
-			</li>
-			<li>
-			  <a href="?event=1">
-				May
-			  </a>
-			</li>
-		  </ul>
-		</div>
-		<div className="prev-list-group__item">
-		  <h3>2016</h3>
-		  <ul className="prev-list">
-			<li>
-			  <a href="?event=1">
-				April
-			  </a>
-			</li>
-			<li>
-			  <a href="?event=1">
-				May
-			  </a>
-			</li>
-		  </ul>
-		</div>
-	  </div>
-	</section>
-
-);
+const PreviousMeetupsList = ({ events }) => {
+  const eventsByYear = groupBy(events, event =>
+    moment(event.time).format('YYYY'));
+  const sortedYears = Object.keys(eventsByYear).sort().reverse();
+  return (
+    <section>
+      <h2 className="label prev-list-title">Previous meetups</h2>
+      <div className="prev-list-group">
+        {sortedYears.map(year => {
+          const events = eventsByYear[year];
+          return (
+            <div key={year} className="prev-list-group__item">
+              <h3>{year}</h3>
+              <ul className="prev-list">
+                {events.map(event => {
+                  const month = moment(event.time).format('MMMM');
+                  return (
+                    <li key={event.id}>
+                      <a href={`?event=${event.id}`}>
+                        {month}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
 export default PreviousMeetupsList;
