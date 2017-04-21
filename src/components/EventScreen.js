268d366421441bf56dab5f62f3071c12cf4110f8
Elliot Waddington <elliotwaddington@havasww.com>
2017-04-21 13:58:03 +0100

    Adding logic to display rsvp button if event is upcoming

--- src/components/EventScreen.js
+++ src/components/EventScreen.js
@@ -2,25 +2,32 @@ import React from 'react';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 import EventRSVPBtn from './EventRSVPBtn';
+import { mockEvents } from '../mocks/events';
 
-const EventScreen = () => (
-  <div className="event-detail">
-    <h1 className="event-detail__title">Event name</h1>
-    <div className="event-detail__info">
-      <div className="event-detail__desc">
-        <p>Event description</p>
-        <p>Another description here</p>
+const EventScreen = ({ eventId }) => {
+  const event = mockEvents[eventId];
+
+  return (
+    <div className="event-detail">
+      <h1 className="event-detail__title">{event.name}</h1>
+      <div className="event-detail__info">
+        <div
+          className="event-detail__desc"
+          dangerouslySetInnerHTML={{ __html: event.description }}
+        />
+
+        {event.status === 'upcoming' ? <EventRSVPBtn /> : ''}
+
       </div>
-      <EventRSVPBtn />
+      <div className="event-detail__comments">
+        <a href="http://meetup.com/blah-blah-blah">
+          View on Meetup.com
+        </a>
+        <CommentBox />
+        <CommentList />
+      </div>
     </div>
-    <div class="event-detail__comments">
-      <a href="http://meetup.com/blah-blah-blah">
-        View on Meetup.com
-      </a>
-      <CommentBox />
-      <CommentList />
-    </div>
-  </div>
-);
+  );
+};
 
 export default EventScreen;
