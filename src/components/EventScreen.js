30e9c708a7f4c317aca6103068e998c96488dca3
shawkes <shawkes@gmail.com>
2017-04-21 14:20:11 +0100

    Comment list

--- src/components/EventScreen.js
+++ src/components/EventScreen.js
@@ -2,25 +2,31 @@ import React from 'react';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 import EventRSVPBtn from './EventRSVPBtn';
+import { mockEvents } from './../mocks/events';
 
-const EventScreen = () => (
-  <div className="event-detail">
-    <h1 className="event-detail__title">Event name</h1>
-    <div className="event-detail__info">
-      <div className="event-detail__desc">
-        <p>Event description</p>
-        <p>Another description here</p>
+const EventScreen = props => {
+  const eventid = props.eventId;
+  const event = mockEvents[eventid];
+  return (
+    <div className="event-detail">
+      <h1 className="event-detail__title">Event name</h1>
+      <div className="event-detail__info">
+        <div className="event-detail__desc">
+          <p>{event.name}</p>
+          <p>{event.description} here</p>
+        </div>
+        {event.status === 'upcoming' && <EventRSVPBtn me={props.me} />}
+
       </div>
-      <EventRSVPBtn />
+      <div className="event-detail__comments">
+        <a href="http://meetup.com/blah-blah-blah">
+          View on Meetup.com
+        </a>
+        <CommentBox />
+        <CommentList />
+      </div>
     </div>
-    <div class="event-detail__comments">
-      <a href="http://meetup.com/blah-blah-blah">
-        View on Meetup.com
-      </a>
-      <CommentBox />
-      <CommentList />
-    </div>
-  </div>
-);
+  );
+};
 
 export default EventScreen;
