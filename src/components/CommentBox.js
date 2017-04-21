4cfe8d7a5f8c37f2469404468cf612cfe5cf39b2
sjalgeo <stephen.algeo@gmail.com>
2017-04-21 16:27:25 +0100

    Make CommentBox a state controlled component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CommentBox = ({ me }) => (
-  <form>
-    {me
-      ? <div>
-          <textarea
-            className="comment-box"
-            placeholder="Chat about this event"
-          />
-          <button type="submit">Send</button>
-        </div>
-      : <p className="note">Please log in to comment.</p>}
-  </form>
-);
+class CommentBox extends Component {
+  state = {
+    commentText: '',
+  };
+
+  updateText = event => {
+    this.setState({ commentText: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.setState({ commentText: '' });
+  };
+
+  render() {
+    // This will become this.props.me when the
+    // parent is ready to pass this prop.
+    const me = {};
+
+    return (
+      <form>
+        {me
+          ? <div>
+              <textarea
+                className="comment-box"
+                placeholder="Chat about this event"
+                value={this.state.commentText}
+                onChange={this.updateText}
+              />
+              <button onClick={this.onSubmit}>Send</button>
+            </div>
+          : <p className="note">Please log in to comment.</p>}
+      </form>
+    );
+  }
+}
 
 export default CommentBox;
