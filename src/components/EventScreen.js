d5ab84c2b1517a63d868a1e4f368cc8559eb020c
Richard Weltman <richard.weltman@wall-e.localdomain>
2017-04-21 13:52:04 +0100

    adds sort ciruit for conditional display of rsvp button

--- src/components/EventScreen.js
+++ src/components/EventScreen.js
@@ -2,18 +2,19 @@ import React from 'react';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 import EventRSVPBtn from './EventRSVPBtn';
-import mockEvents from '../mocks/events'
+import {mockEvents} from '../mocks/events'
 
 const EventScreen = ({ eventId }) => {
+  const event = mockEvents[eventId];
   return (
     <div className="event-detail">
-      <h1 className="event-detail__title">Event name</h1>
+      <h1 className="event-detail__title">{event.name}</h1>
       <div className="event-detail__info">
-        <div className="event-detail__desc">
-          <p>Event description</p>
-          <p>Another description here</p>
-        </div>
-        <EventRSVPBtn />
+        <div 
+          className="event-detail__desc"
+          danegrouslySetInnerHTML={{ __html : event.description }} 
+        />
+        {event.status !== 'past' && <EventRSVPBtn />}
       </div>
       <div class="event-detail__comments">
         <a href="http://meetup.com/blah-blah-blah">
