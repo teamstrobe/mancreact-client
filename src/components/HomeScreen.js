09f37eb841e54a92e74dfb2775a3089cc8ce6b37
Rueben <ruebencee@gmail.com>
2017-04-21 15:48:24 +0100

    adding datetime to upcomingeventhero

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -6,14 +6,21 @@ import { mockEventsList } from '../mocks/events';
 const HomeScreen = () => {
   const events = mockEventsList;
   let upcomingEvent;
+  let upcomingDateTime;
   let previousEvents;
   if (events != null) {
     upcomingEvent = events.filter(event => event.status === 'upcoming')[0];
     previousEvents = events.filter(event => event.status === 'past');
+    upcomingDateTime = new Date(upcomingEvent.time).toLocaleDateString();
   }
   return (
     <div>
-      <UpcomingEventHero event={upcomingEvent} />
+      <UpcomingEventHero
+        mainTitle={upcomingEvent.name}
+        description={upcomingEvent.description}
+        date={upcomingDateTime}
+      />
+
       <PreviousMeetupsList events={previousEvents} />
     </div>
   );
--- src/components/UpcomingEventHero.js
+++ src/components/UpcomingEventHero.js
@@ -4,21 +4,19 @@ const UpcomingEventHero = props => {
   const heroStyles = {
     backgroundImage: 'url(https://pbs.twimg.com/profile_banners/14314572/1462486903/1500x500)',
   };
+  // console.log(upcomingEvent);
   return (
     <article className="upcoming" style={heroStyles}>
       <h1>
         <a href="/event.html">
           {props.mainTitle}
           {' '}
-          <span className="upcoming-title-sub">{props.subTitle}</span>
+          <span className="upcoming-title-sub">Sub-title</span>
         </a>
       </h1>
       <h2 className="upcoming-by">{props.narrator}</h2>
-      <div className="upcoming-date">{props.comingDate}</div>
-      <div>
-        <p>Description here</p>
-        <p>Another paragraph</p>
-      </div>
+      <div className="upcoming-date">{props.date}</div>
+      <div dangerouslySetInnerHTML={{ __html: props.description }} />
     </article>
   );
 };
