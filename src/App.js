ace465d1f6a08cbc665851049c94738462440c2f
Joe Critchley <joecritch@gmail.com>
2017-04-21 11:32:46 +0100

    Added Footer.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Footer from './Footer';
 
 const App = () => (
   <div>
-    Yo.
+    <Footer />
   </div>
 );
 
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="container">
+      Organised & sponsored by <a href="http://strobedigital.com">Strobe</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
