5a58dada26bdf7b1d5a4d151afab42b00817f9bb
Richard Weltman <richard.weltman@wall-e.creativelynx.ad>
2017-04-21 12:21:59 +0100

    adds ternary logic to comment box

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
-const CommentBox = () => (
+const CommentBox = ({me}) => (
   <form>
-    {/* if they're not logged in 
-            <p className="note">Please log in to comment.</p>
-        */}
-    <div>
-      <textarea className="comment-box" placeholder="Chat about this event" />
-      <button type="submit">Send</button>
-    </div>
+    {me
+    	?
+		<div>
+		  <textarea
+			className="comment-box"
+			placeholder="Chat about this event"
+		  />
+		  <button type="submit">Send</button>
+		</div>
+	: <p className="note">Please log in to commnet.</p>}
   </form>
 );
 
