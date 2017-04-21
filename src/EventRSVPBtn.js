8ebfe8eeb405143131d479327eb09ba59bc876a2
shawkes <shawkes@gmail.com>
2017-04-21 12:14:14 +0100

    Logic in RSVP

--- src/EventRSVPBtn.js
+++ src/EventRSVPBtn.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class EventRSVPBtn extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const user = this.props.user;
     return (
       <div className="rsvp-btn-group">
-        <button>
-          Say I'm going
-        </button>
+        {!user
+          ? <p class="note">
+              Please log in to RSVP.
+            </p>
+          : <button>
+              Say I'm going
+            </button>}
       </div>
     );
   }
 }
 
+EventRSVPBtn.propTypes = {
+  user: PropTypes.any.isRequired,
+};
 export default EventRSVPBtn;
