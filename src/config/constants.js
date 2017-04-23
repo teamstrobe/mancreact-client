20c45870afa420978ff998ab13ec774c11c163c7
Joe Critchley <joecritch@gmail.com>
2017-04-23 21:10:22 +0100

    Changed REACT_APP_REDIRECT_URI

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 export const MEETUP_AUTH_URL = 'https://secure.meetup.com/oauth2/authorize';
 export const CLIENT_ID = 'cfo6m3ac1haj5pdbn20b3phugt';
-export const MEETUP_REDIRECT_URI = 'http://localhost:3000';
+export const MEETUP_REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI || 'https://mancreact.com';
 export const MANCREACT_URL_NAME = 'Manchester-React-User-Group';
 export const MANCREACT_SERVER_HOST =
   process.env.REACT_APP_MANCREACT_SERVER_HOST ||
